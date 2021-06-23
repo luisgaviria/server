ed7ef73843224521af1e163bcf8b52f55b5f63c7
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-06-23 09:29:37 -0400

    new edits

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-});
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(express.json());
-
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -28,11 +25,15 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
 
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
